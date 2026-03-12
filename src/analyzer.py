fbf81611a8be4d127dc7f5060c9bacb2a7f75cd6
Mordy Pesach <mordypassover@gmail.com>
2026-03-12 13:38:17 +0200

    add detect camera switches

--- src/analyzer.py
+++ src/analyzer.py
@@ -31,7 +31,21 @@ def date_range(images_data):# בודק טווח זמן מחזיר תאריך ת�
 
 
 def detect_camera_switches(images_data):
-    pass
+    sorted_images = sorted(
+        [img for img in images_data if img["datetime"]],
+        key=lambda x: x["datetime"]
+    )
+    switches = []
+    for i in range(1, len(sorted_images)):
+        prev_cam = sorted_images[i - 1].get("camera_model")
+        curr_cam = sorted_images[i].get("camera_model")
+        if prev_cam and curr_cam and prev_cam != curr_cam:
+            switches.append({
+                "date": sorted_images[i]["datetime"],
+                "from": prev_cam,
+                "to": curr_cam
+            })
+    return switches
 
 def pictures_in_range_detection(images_data,rad_range):
     pass
